821aaad76f16a7b2a0f20982c44cf110d18234cf
roysatadru <satadru.roy98@gmail.com>
2023-08-06 12:20:01 +0530

    🐛 fix: reinitialized the table after dropping the table

--- src/init_table.sql
+++ src/init_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE sales_table (
   date_fulfilled DATE,
   customer_name VARCHAR(300) NOT NULL,
   product_name VARCHAR(300) NOT NULL,
-  VOLUME NUMERIC(10,3) NOT NULL CHECK (volume >= 0),
+  volume NUMERIC(10,3) NOT NULL CHECK (volume >= 0),
   is_recurring BOOLEAN DEFAULT FALSE,
   is_disputed BOOLEAN DEFAULT FALSE
 );

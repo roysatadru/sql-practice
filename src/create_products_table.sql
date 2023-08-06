0468b3ce085d7b2d2b4d9d21d39cd4059151b556
roysatadru <satadru.roy98@gmail.com>
2023-08-06 12:11:44 +0530

    ✨ feat: initialized sales_db and sales_table

--- src/create_products_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-  name VARCHAR(200) NOT NULL,
-  price NUMERIC(10, 2),
-  description TEXT,
-  amount_in_stock SMALLINT,
-  image_url VARCHAR(500)
-);
---
+++ src/init_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales_table (
+  id INT PRIMARY KEY AUTO_INCREMENt,
+  date_created DATE DEFAULT (CURRENT_DATE),
+  date_fulfilled DATE,
+  customer_name VARCHAR(300) NOT NULL,
+  product_name VARCHAR(300) NOT NULL,
+  VOLUME NUMERIC(10,3) NOT NULL CHECK (volume >= 0),
+  is_recurring BOOLEAN DEFAULT FALSE,
+  is_disputed BOOLEAN DEFAULT FALSE
+);

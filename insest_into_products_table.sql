29c6adaa681b1f322776b5d8d1a4577408a7e0d0
roysatadru <satadru.roy98@gmail.com>
2023-08-06 05:44:39 +0530

    ✨ feat(insest_into_products_table.sql): inserted into products table

---
+++ insest_into_products_table.sql
@@ -0,0 +1,14 @@
+INSERT INTO products (
+    name,
+    price,
+    description,
+    amount_in_stock,
+    image_url
+  )
+VALUES (
+    'Book',
+    49.99,
+    'This is an amazing book and this is not the only thing which describes the book.',
+    8,
+    '/uploads/media/book.jpg'
+  );
